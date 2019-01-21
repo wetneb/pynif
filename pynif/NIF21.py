749134e0b4787aba32d086b04b87eb661fad1b2c
Antonin Delpeuch <antonin@delpeuch.eu>
2019-01-21 17:50:51 +0100

    Adapt root NIF21 wrapper to new structure

--- pynif/NIF21.py
+++ pynif/NIF21.py
@@ -5,7 +5,7 @@ from prefixes import NIFPrefixes
 
 class NIF21():
     def __init__(self):
-        self._obj_beans = []
+        self._obj_contextes = []
         self._obj_prefixes = NIFPrefixes()
 
     def context(self, baseURI, beginIndex, endIndex, mention):
@@ -14,23 +14,15 @@ class NIF21():
         self._obj_context.beginIndex = beginIndex
         self._obj_context.endIndex = endIndex
         self._obj_context.mention = mention
+        self._obj_contextes.append(self._obj_context)
 
     def bean(self, mention, beginIndex, endIndex, taClassRef, score, annotator, taIdentRef, taMsClassRef):
-        bean = NIFBean()
-        bean.referenceContext = self._obj_context.referenceContext
-        bean.context = self._obj_context.baseURI
-        bean.mention = mention
-        bean.beginIndex = beginIndex
-        bean.endIndex = endIndex
+        bean = self._obj_context.addBean(beginIndex, endIndex)
         bean.taClassRef = taClassRef
         bean.score = score
         bean.annotator = annotator
         bean.taIdentRef = taIdentRef
         bean.taMsClassRef = taMsClassRef
-        self._obj_beans.append(bean)
 
     def turtle(self):
-        result = ''.join([self._obj_prefixes.turtle, '\n', self._obj_context.turtle, '\n\n'])
-        for currentBean in enumerate(self._obj_beans):
-            result += currentBean[1].turtle + '\n\n'
-        return result
+        return self._obj_prefixes.turtle + '\n' + '\n\n'.join(c.turtle for c in self._obj_contextes)
