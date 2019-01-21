0c4266ddbc7040645bb1f90ac931ddd4a926030b
Antonin Delpeuch <antonin@delpeuch.eu>
2019-01-21 14:31:12 +0100

    Rename files

--- pynif/NIF21.py
+++ pynif/NIF21.py
@@ -1,22 +1,22 @@
-import NIFBean
-import NIFContext
-import NIFPrefixes
+from bean import NIFBean
+from context import NIFContext
+from prefixes import NIFPrefixes
 
 
 class NIF21():
     def __init__(self):
         self._obj_beans = []
-        self._obj_prefixes = NIFPrefixes.NIFPrefixes()
+        self._obj_prefixes = NIFPrefixes()
 
     def context(self, baseURI, beginIndex, endIndex, mention):
-        self._obj_context = NIFContext.NIFContext()
+        self._obj_context = NIFContext()
         self._obj_context.baseURI = baseURI
         self._obj_context.beginIndex = beginIndex
         self._obj_context.endIndex = endIndex
         self._obj_context.mention = mention
 
     def bean(self, mention, beginIndex, endIndex, taClassRef, score, annotator, taIdentRef, taMsClassRef):
-        bean = NIFBean.NIFBean()
+        bean = NIFBean()
         bean.referenceContext = self._obj_context.referenceContext
         bean.context = self._obj_context.baseURI
         bean.mention = mention
