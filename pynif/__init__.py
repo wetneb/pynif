02660c39f2992cf127e99a2b724be8559239974e
Antonin Delpeuch <antonin@delpeuch.eu>
2019-01-22 17:11:17 +0100

    Expose main classes in root package

--- pynif/__init__.py
+++ pynif/__init__.py
@@ -1,1 +1,8 @@
 name = "pynif"
+
+from .phrase import NIFPhrase
+from .context import NIFContext
+from .collection import NIFCollection
+from .prefixes import NIFPrefixes
+
+__all__ = [ 'NIFPhrase', 'NIFContext', 'NIFCollection', 'NIFPrefixes' ]
